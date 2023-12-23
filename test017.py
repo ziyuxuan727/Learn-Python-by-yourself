748b41809ba6e1c74edce2a2597e412c4fe64f2a
紫宇轩727 <144686881+ziyuxuan727@users.noreply.github.com>
2023-12-23 23:28:27 +0800

    Add files via upload

---
+++ test017.py
@@ -0,0 +1,21 @@
+# 导包，导入Line功能构建折线图对象
+from pyecharts.charts import Line
+from pyecharts.options import TitleOpts, LegendOpts, ToolboxOpts, TooltipOpts, VisualMapOpts
+
+# 得到折线图对象
+line = Line()
+# 添加x轴数据
+line.add_xaxis(["中国", "美国", "日本"])
+# 添加y轴数据
+line.add_yaxis("GDP", [30, 20, 10])
+
+# 设置全局配置项，通过set_global_opts来设置
+line.set_global_opts(
+    title_opts=TitleOpts(title="GDP展示", pos_left="center", pos_bottom="1%"),
+    legend_opts=LegendOpts(is_show=True),
+    toolbox_opts=ToolboxOpts(is_show=True),
+    visualmap_opts=VisualMapOpts(is_show=True),
+)
+
+# 生成图表
+line.render()
